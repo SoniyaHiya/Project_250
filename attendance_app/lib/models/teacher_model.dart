e0941110cf79b3c5dd8a183b5f15a7a1c40886d7
SoniyaHiya <soniyamubasshir60@gmail.com>
2026-07-14 09:53:59 +0600

    add teacher model

---
+++ attendance_app/lib/models/teacher_model.dart
@@ -0,0 +1,13 @@
+class TeacherModel {
+  final String teacherId;
+  final String name;
+  final String email;
+  final String password;
+
+  TeacherModel({
+    required this.teacherId,
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+}
