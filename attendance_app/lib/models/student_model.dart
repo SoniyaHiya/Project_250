10e09aee2ad2bfdb5736d80bb01ba21f76205b2c
SoniyaHiya <soniyamubasshir60@gmail.com>
2026-07-14 09:55:41 +0600

    add student model

---
+++ attendance_app/lib/models/student_model.dart
@@ -0,0 +1,15 @@
+class StudentModel {
+  final String studentId;
+  final String name;
+  final String department;
+  final String batch;
+  final String deviceId;
+
+  StudentModel({
+    required this.studentId,
+    required this.name,
+    required this.department,
+    required this.batch,
+    required this.deviceId,
+  });
+}
