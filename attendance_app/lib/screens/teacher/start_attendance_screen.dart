8df79fc020d8b9fae67bbf474e2ffeda6d7910eb
SoniyaHiya <soniyamubasshir60@gmail.com>
2026-07-14 10:25:59 +0600

    Create start_attendance_screen.dart

---
+++ attendance_app/lib/screens/teacher/start_attendance_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'start_attendance_screen.dart';
+
+class StartAttendanceScreen extends StatelessWidget {
+  const StartAttendanceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Start Attendance"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            const Text(
+              "Course",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            const TextField(
+              decoration: InputDecoration(
+                hintText: "Enter Course Code",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            const Text(
+              "Room",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            const TextField(
+              decoration: InputDecoration(
+                hintText: "Enter Room Number",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => const StartAttendanceScreen(),
+    ),
+  );
+},
+                child: const Text("Start Session"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
