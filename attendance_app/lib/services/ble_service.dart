49e07d3d3ab7552d1ab921163e92fe95199b68d8
SoniyaHiya <soniyamubasshir60@gmail.com>
2026-07-15 00:17:30 +0600

    Bluetooth ON/OFF Check Method

--- attendance_app/lib/services/ble_service.dart
+++ attendance_app/lib/services/ble_service.dart
@@ -2,4 +2,8 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class BleService {
 
+Future<bool> isBluetoothOn() async {
+    return await FlutterBluePlus.adapterState.first ==
+        BluetoothAdapterState.on;
+  }
 }
